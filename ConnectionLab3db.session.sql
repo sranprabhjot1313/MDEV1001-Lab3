651270036b9fc23405d624bdd00b14118495115f
sranprabhjot1313 <sranprabhjot1313@gmail.com>
2025-03-25 15:01:04 -0400

    first commit

---
+++ ConnectionLab3db.session.sql
@@ -0,0 +1,26 @@
+INSERT INTO Movies (
+    title,
+    studio,
+    genres,
+    directors,
+    writers,
+    actors,
+    year,
+    length,
+    shortDescription,
+    mpaRating,
+    criticsRating
+  )
+VALUES (
+    'Joker',
+    'Warner Bros',
+    'Drama, Thriller',
+    'Todd Phillips',
+    'Todd Phillips, Scott Silver',
+    'Joaquin Phoenix, Robert De Niro',
+    2019,
+    122,
+    'Arthur Fleck, a party clown and a failed stand-up comedian, leads an impoverished life with his ailing mother. However, when society shuns him and brands him as a freak, he decides to embrace the life of chaos in Gotham City',
+    'R',
+    8.5
+  );
